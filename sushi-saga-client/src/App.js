3e87b7956511dd9fd6b67136e0a2dd05bde2ff44
Jake <jakelovitz@gmail.com>
2019-07-02 09:43:20 -0400

    set up fetch request within componentDidMount

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -5,9 +5,21 @@ import Table from './containers/Table';
 // Endpoint!
 const API = "http://localhost:3000/sushis"
 
+
 class App extends Component {
+  
+  state = {
+    sushi: []
+  }
+
+  componentDidMount() {
+    fetch(API)
+      .then(response => response.json())
+      .then(data => this.setState({ sushi: data }));
+  }
 
   render() {
+    console.log(this.state)
     return (
       <div className="app">
         <SushiContainer  />
